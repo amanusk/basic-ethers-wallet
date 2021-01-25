b82b8d3c9fe2fef8cb787d8a38ba1e37903e4927
amanusk <amanusk@kzencorp.com>
2021-01-25 14:58:25 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,90 @@
+import { ethers, Wallet } from 'ethers';
+import * as dotenv from 'dotenv';
+import { program } from 'commander';
+import { ERC20_INTERFACE } from './abis/ERC20-interface';
+
+const provider = new ethers.providers.InfuraProvider(
+  // This should be mainnet, kovan, rikenby or ropsten
+  'kovan',
+  process.env.INFURA_API_KEY,
+);
+
+function getWallet(): Wallet {
+  let mnemonic = process.env.MNEMONIC;
+  if (mnemonic == null) {
+    console.log(
+      'THIS IS A TEMP ADDRESS. Please fill in the MNEMONIC field in the .env file',
+    );
+    let wallet = Wallet.createRandom();
+    let mnemonic = wallet.mnemonic;
+    console.log('12-word seed: ' + mnemonic.phrase);
+    return wallet;
+  } else {
+    let wallet = Wallet.fromMnemonic(mnemonic).connect(provider);
+    return wallet;
+  }
+}
+
+program.command('address').action(async (options) => {
+  let wallet = getWallet();
+  console.log(wallet.address);
+});
+
+program
+  .command('balance [address]')
+  .action(async (address: string, options) => {
+    if (address == null) {
+      address = getWallet().address;
+    }
+    let balance = await provider.getBalance(address);
+    console.log(ethers.utils.formatEther(balance));
+  });
+
+program
+  .command('transfer <to> <amount>')
+  .action(async (to: string, amount: string, options) => {
+    let wallet = getWallet();
+    let tx = await wallet.sendTransaction({
+      to,
+      value: ethers.utils.parseEther(amount),
+    });
+    console.log(
+      'Transaction at: ' + 'https://kovan.etherscan.io/tx/' + tx.hash,
+    );
+    await tx.wait(1);
+    console.log('Tx mined');
+  });
+
+program
+  .command('transfer_token <token> <to> <amount>')
+  .action(async (token: string, to: string, amount: string, options) => {
+    let wallet = getWallet();
+
+    let erc20 = new ethers.Contract(token, ERC20_INTERFACE, wallet);
+
+    let decimals = await erc20.decimals();
+    const amountHex = ethers.utils.parseUnits(amount, decimals);
+
+    let tx = await erc20.transfer(to, amountHex);
+    console.log(
+      'Transaction at: ' + 'https://kovan.etherscan.io/tx/' + tx.hash,
+    );
+    await tx.wait(1);
+    console.log('Tx mined');
+  });
+
+program
+  .command('balance_token <token> [address]')
+  .action(async (token: string, address: string, options) => {
+    if (address == null) {
+      address = getWallet().address;
+    }
+    let erc20 = new ethers.Contract(token, ERC20_INTERFACE, provider);
+    const balance = await erc20.balanceOf(address);
+    let decimals = await erc20.decimals();
+
+    console.log('Balance:', ethers.utils.formatUnits(balance, decimals));
+  });
+
+dotenv.config();
+program.parse(process.argv);
