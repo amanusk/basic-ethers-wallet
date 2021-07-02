8b2669b74bdfce3fe334805137abe2ee509fca02
Alex Manuskin <alex@manuskin.org>
2021-07-02 13:58:14 +0300

    Add README.md

---
+++ README.md
@@ -0,0 +1,56 @@
+# Basic Ethers wallet
+
+Basic cli wrapper to work with the ethres wallet class. Basic supported funcitonality:
+
+- Get balance of address ETH + token
+- Send ETH/Token
+- Sign message
+
+## Installation
+
+```
+yarn install
+yarn build
+```
+
+## Running commands
+
+Get wallet address
+
+```
+ts-node ./src/index.ts address
+```
+
+Get wallet or any other address balance
+
+```
+ts-node ./src/index.ts balance [address]
+```
+
+Get ERC20 token balance
+
+```
+ts-node ./src/index.ts balance_token <token_address> [address]
+```
+
+Transfer ETH from wallet
+
+```
+ts-node ./src/index.ts transfer <to> <amount>
+```
+
+Transfer ERC20 from wallet
+
+```
+ts-node ./src/index.ts transfer_token <token_address> <to> <amount>
+```
+
+## Thanks
+
+If you like it than you soulda put a start ⭐ on it
+
+Twitter: [@amanusk\_][https://twitter.com/amanusk_]
+
+## License
+
+MIT
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,31 @@
-import { ethers, Wallet } from 'ethers';
-import * as dotenv from 'dotenv';
-import { program } from 'commander';
-import { ERC20_INTERFACE } from './abis/ERC20-interface';
+import { ethers, Wallet } from "ethers";
+import * as dotenv from "dotenv";
+import { program } from "commander";
+import { ERC20_INTERFACE } from "./abis/ERC20-interface";
 
-const provider = new ethers.providers.InfuraProvider(
-  // This should be mainnet, kovan, rikenby or ropsten
-  'kovan',
-  process.env.INFURA_API_KEY,
-);
+dotenv.config();
 
-// let url = 'http://192.168.1.25:18545';
-// let provider = new ethers.providers.JsonRpcProvider(url);
+const ETHEREUM_RPC_URL = process.env.ETHEREUM_RPC_URL || "http://127.0.0.1:8545";
+console.log(ETHEREUM_RPC_URL);
+
+const NETWORK = process.env.NETWORK || "";
+
+let provider = new ethers.providers.JsonRpcProvider(ETHEREUM_RPC_URL);
+
+// Uncomment to use INFURA provider
+// const provider = new ethers.providers.InfuraProvider(
+//   // This should be mainnet, kovan, rikenby or ropsten
+//   NETWORK,
+//   process.env.INFURA_API_KEY,
+// );
 
 function getWallet(): Wallet {
   let mnemonic = process.env.MNEMONIC;
   if (mnemonic == null) {
-    console.log(
-      'THIS IS A TEMP ADDRESS. Please fill in the MNEMONIC field in the .env file',
-    );
+    console.log("THIS IS A TEMP ADDRESS. Please fill in the MNEMONIC field in the .env file");
     let wallet = Wallet.createRandom();
     let mnemonic = wallet.mnemonic;
-    console.log('12-word seed: ' + mnemonic.phrase);
+    console.log("12-word seed: " + mnemonic.phrase);
     return wallet;
   } else {
     let wallet = Wallet.fromMnemonic(mnemonic).connect(provider);
@@ -28,38 +33,39 @@ function getWallet(): Wallet {
   }
 }
 
-program.command('address').action(async (options) => {
+program.command("address").action(async options => {
   let wallet = getWallet();
   console.log(wallet.address);
 });
 
-program
-  .command('balance [address]')
-  .action(async (address: string, options) => {
-    if (address == null) {
-      address = getWallet().address;
-    }
-    let balance = await provider.getBalance(address);
-    console.log(ethers.utils.formatEther(balance));
-  });
+program.command("balance [address]").action(async (address: string, options) => {
+  if (address == null) {
+    address = getWallet().address;
+  }
+  let balance = await provider.getBalance(address);
+  console.log(ethers.utils.formatEther(balance));
+});
 
-program
-  .command('transfer <to> <amount>')
-  .action(async (to: string, amount: string, options) => {
-    let wallet = getWallet();
-    let tx = await wallet.sendTransaction({
+program.command("transfer <to> <amount>").action(async (to: string, amount: string, options) => {
+  let wallet = getWallet();
+  let feeData = await provider.getFeeData();
+  let nonce = await wallet.getTransactionCount();
+  if (feeData.maxPriorityFeePerGas !== null && feeData.maxFeePerGas !== null) {
+    let tx = await wallet.populateTransaction({
       to,
       value: ethers.utils.parseEther(amount),
+      nonce,
     });
-    console.log(
-      'Transaction at: ' + 'https://kovan.etherscan.io/tx/' + tx.hash,
-    );
-    await tx.wait(1);
-    console.log('Tx mined');
-  });
+    console.log("Tx", tx);
+    let tx_rec = await wallet.sendTransaction(tx);
+    console.log(`Transaction at: https://${NETWORK}.etherscan.io/tx/${tx_rec.hash}`);
+    await tx_rec.wait(1);
+    console.log("Tx mined");
+  }
+});
 
 program
-  .command('transfer_token <token> <to> <amount>')
+  .command("transfer_token <token> <to> <amount>")
   .action(async (token: string, to: string, amount: string, options) => {
     let wallet = getWallet();
 
@@ -69,34 +75,29 @@ program
     const amountHex = ethers.utils.parseUnits(amount, decimals);
 
     let tx = await erc20.transfer(to, amountHex);
-    console.log(
-      'Transaction at: ' + 'https://kovan.etherscan.io/tx/' + tx.hash,
-    );
+    console.log(`Transaction at: https://${NETWORK}.etherscan.io/tx/${tx.hash}`);
     await tx.wait(1);
-    console.log('Tx mined');
+    console.log("Tx mined");
   });
 
-program
-  .command('balance_token <token> [address]')
-  .action(async (token: string, address: string, options) => {
-    if (address == null) {
-      address = getWallet().address;
-    }
-    let erc20 = new ethers.Contract(token, ERC20_INTERFACE, provider);
-    const balance = await erc20.balanceOf(address);
-    let decimals = await erc20.decimals();
+program.command("balance_token <token> [address]").action(async (token: string, address: string, options) => {
+  if (address == null) {
+    address = getWallet().address;
+  }
+  let erc20 = new ethers.Contract(token, ERC20_INTERFACE, provider);
+  const balance = await erc20.balanceOf(address);
+  let decimals = await erc20.decimals();
 
-    console.log('Balance:', ethers.utils.formatUnits(balance, decimals));
-  });
-
-program.command('sign_message <data>').action(async (data: string, options) => {
-  let wallet = await getWallet();
-  let signature = await wallet.signMessage(data);
-  console.log('Sig', signature);
-
-  let recoveredAddress = ethers.utils.verifyMessage(data, signature);
-  console.log('Address', recoveredAddress);
+  console.log("Token Balance:", ethers.utils.formatUnits(balance, decimals));
 });
 
-dotenv.config();
+program.command("sign_message <data>").action(async (data: string, options) => {
+  let wallet = getWallet();
+  let signature = await wallet.signMessage(data);
+  console.log("Sig", signature);
+
+  let recoveredAddress = ethers.utils.verifyMessage(data, signature);
+  console.log("Address", recoveredAddress);
+});
+
 program.parse(process.argv);
